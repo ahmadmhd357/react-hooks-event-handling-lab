b96a3e8b0c805391aad7963eff414ca07b1329d9
Ahmad Al Hariri <ahmadmhd357@gmail.com>
2022-11-27 17:12:56 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handlFoucs(){
+        console.log('Good!')
+    }
+    function handlBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return <button onFocus={handlFoucs} onBlur={handlBlur}> Eyes on me</button>
+        
+   
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handlChange(){
+       console.log('Entering password...')
+    }
     return (
-        <div></div>
+       
+            <input type='password' placeholder="Enter Password" onChange={handlChange}></input>
+        
     )
 }
 
